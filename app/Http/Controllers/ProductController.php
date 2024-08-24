f4fe468b7d144d53daefac07dfc055c238ee8362
QuitoTactico <teteban0917@gmail.com>
2024-08-23 22:23:55 -0500

    After Pint

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,27 +1,27 @@
-<?php 
- 
-namespace App\Http\Controllers; 
- 
-use Illuminate\Http\Request; 
-use Illuminate\View\View; 
-use Illuminate\Http\RedirectResponse;
-use App\Models\Product; 
- 
-class ProductController extends Controller 
-{
+<?php
 
-    public function index(): View 
-    { 
-        $viewData = []; 
-        $viewData["title"] = "Products - Online Store"; 
-        $viewData["subtitle"] =  "List of products"; 
-        $viewData["products"] = Product::all();
-        return view('product.index')->with("viewData", $viewData); 
-    } 
- 
-    public function show(string $id) : View | RedirectResponse
-    { 
-        $viewData = []; 
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class ProductController extends Controller
+{
+    public function index(): View
+    {
+        $viewData = [];
+        $viewData['title'] = 'Products - Online Store';
+        $viewData['subtitle'] = 'List of products';
+        $viewData['products'] = Product::all();
+
+        return view('product.index')->with('viewData', $viewData);
+    }
+
+    public function show(string $id): View|RedirectResponse
+    {
+        $viewData = [];
 
         try {
             $product = Product::findOrFail($id);
@@ -29,40 +29,42 @@ class ProductController extends Controller
             return redirect()->route('home.index');
         }
 
-        $viewData["title"] = $product["name"]." - Online Store"; 
-        $viewData["subtitle"] = $product["name"]." - Product information"; 
-        $viewData["product"] = $product; 
-        return view('product.show')->with("viewData", $viewData); 
-    } 
+        $viewData['title'] = $product['name'].' - Online Store';
+        $viewData['subtitle'] = $product['name'].' - Product information';
+        $viewData['product'] = $product;
 
-    public function create(): View 
-    { 
-        $viewData = []; //to be sent to the view 
-        $viewData["title"] = "Create product"; 
- 
-        return view('product.create')->with("viewData",$viewData); 
-    } 
- 
-    public function save(Request $request) 
-    { 
-        $request->validate([ 
-            "name" => "required", 
-            "price" => "required|numeric|min:0" 
-        ]); 
+        return view('product.show')->with('viewData', $viewData);
+    }
+
+    public function create(): View
+    {
+        $viewData = []; //to be sent to the view
+        $viewData['title'] = 'Create product';
+
+        return view('product.create')->with('viewData', $viewData);
+    }
+
+    public function save(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'price' => 'required|numeric|min:0',
+        ]);
         //dd($request->all()); //dump the request data
-        //here will be the code to call the model and save it to the database 
+        //here will be the code to call the model and save it to the database
 
-        Product::create($request->only(["name","price"])); 
- 
-        #return back(); 
+        Product::create($request->only(['name', 'price']));
+
+        //return back();
         return redirect()->route('product.success');
     }
 
-    public function success(): View 
-    { 
-        $viewData = []; 
-        $viewData["title"] = "Products - Online Store"; 
-        $viewData["subtitle"] =  "Successfully created product"; 
-        return view('product.success')->with("viewData", $viewData); 
-    } 
-} 
+    public function success(): View
+    {
+        $viewData = [];
+        $viewData['title'] = 'Products - Online Store';
+        $viewData['subtitle'] = 'Successfully created product';
+
+        return view('product.success')->with('viewData', $viewData);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -8,45 +8,47 @@ Route::get('/', function () {
 });
 **/
 
-Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index");
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index');
 
-Route::get('/about', function () { 
-    $data1 = "About us - Online Store"; 
-    $data2 = "About us"; 
-    $description = "This is an about page ..."; 
-    $author = "Developed by: Esteban Vergara Giraldo"; 
-    return view('home.about')->with("title", $data1) 
-      ->with("subtitle", $data2) 
-      ->with("description", $description) 
-      ->with("author", $author); 
-})->name("home.about"); 
+Route::get('/about', function () {
+    $data1 = 'About us - Online Store';
+    $data2 = 'About us';
+    $description = 'This is an about page ...';
+    $author = 'Developed by: Esteban Vergara Giraldo';
 
-Route::get('/contact', function () { 
-    $data1 = "Contact us - Online Store"; 
-    $data2 = "Contact us"; 
-    $email = "miau@gmail.com"; 
-    $address = "Cárcel Bellavista: Diagonal 44 # 39 - 145 Barrio Las Vegas, Bello (Antioquia)"; 
-    $phone_number = "666 666 6666"; 
-    return view('home.contact')->with("title", $data1) 
-      ->with("subtitle", $data2) 
-      ->with("email", $email) 
-      ->with("address", $address) 
-      ->with("phone_number", $phone_number); 
-})->name("home.contact"); 
+    return view('home.about')->with('title', $data1)
+        ->with('subtitle', $data2)
+        ->with('description', $description)
+        ->with('author', $author);
+})->name('home.about');
 
-# ------------PRODUCTS-----------------
+Route::get('/contact', function () {
+    $data1 = 'Contact us - Online Store';
+    $data2 = 'Contact us';
+    $email = 'miau@gmail.com';
+    $address = 'Cárcel Bellavista: Diagonal 44 # 39 - 145 Barrio Las Vegas, Bello (Antioquia)';
+    $phone_number = '666 666 6666';
+
+    return view('home.contact')->with('title', $data1)
+        ->with('subtitle', $data2)
+        ->with('email', $email)
+        ->with('address', $address)
+        ->with('phone_number', $phone_number);
+})->name('home.contact');
+
+// ------------PRODUCTS-----------------
 
 //why do the routes order matter?
 
-Route::get('/products', 'App\Http\Controllers\ProductController@index')->name("product.index"); 
+Route::get('/products', 'App\Http\Controllers\ProductController@index')->name('product.index');
 
-Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name("product.create"); 
+Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name('product.create');
 
-Route::post('/products/save', 'App\Http\Controllers\ProductController@save')->name("product.save");
+Route::post('/products/save', 'App\Http\Controllers\ProductController@save')->name('product.save');
 
-Route::get('/products/success', 'App\Http\Controllers\ProductController@success')->name("product.success");
+Route::get('/products/success', 'App\Http\Controllers\ProductController@success')->name('product.success');
 
 //i see now why it's important.
 //always keep this one at the end
 
-Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name("product.show");
+Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name('product.show');
