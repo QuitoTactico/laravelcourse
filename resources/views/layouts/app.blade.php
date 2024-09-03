e254bd9e088537b6fbca9cbe32d6972345bf3313
QuitoTactico <teteban0917@gmail.com>
2024-09-03 11:16:48 -0500

    Navbar shortcuts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,6 +23,9 @@ target="#navbarNavAltMarkup"
           <a class="nav-link active" href="{{ route('home.index') }}">Home</a> 
           <a class="nav-link active" href="{{ route('product.index') }}">Products</a> 
           <a class="nav-link active" href="{{ route('product.create') }}">Create</a> 
+          <a class="nav-link active" href="{{ route('cart.index') }}">Cart</a> 
+          <a class="nav-link active" href="{{ route('image.index') }}">Image</a> 
+          <a class="nav-link active" href="{{ route('imagenotdi.index') }}">ImageNotDi</a> 
           <a class="nav-link active" href="{{ route('home.about') }}">About</a> 
           <a class="nav-link active" href="{{ route('home.contact') }}">Contact</a> 
         </div> 
